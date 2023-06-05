f8c8eabb45f3c8247a2411f7672164125e39e4ca
Alexander Moreno Ramos <alexander.moreno@inalambria.com>
2023-06-05 10:37:18 -0500

    Add project files.

---
+++ SocketClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using SockNet.ClientSocket;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketClientExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        private static async Task MainAsync()
+        {
+            byte[] recData = null;
+            SocketClient client = new SocketClient("127.0.0.1", 9999);
+            try
+            {
+                int salir = 0;
+                while (salir == 0)
+                {
+                    Console.WriteLine("Escriba el mensaje:");
+                    var message = Console.ReadLine();
+                    if (await client.Connect())
+                    {
+                        await client.Send(message);
+                        recData = await client.ReceiveBytes();
+                    }
+
+                    Console.WriteLine("Received data: " + Encoding.UTF8.GetString(recData));
+                    Console.WriteLine("Desea salir?");
+                    Console.WriteLine("0: NO");
+                    Console.WriteLine("1: SI");
+
+                    salir = int.Parse(Console.ReadLine());
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception raised: " + e);
+            }
+            //...
+            client.Disconnect();
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using SockNet.ServerSocket;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServerExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ip = "127.0.0.1";
+            int port = 9999;
+            var socketServer = new SocketServer();
+            socketServer.InitializeSocketServer(ip, 9999);
+            socketServer.SetReaderBufferBytes(1024);
+            socketServer.StartListening();
+            Console.WriteLine($"Socket Listent on Ip: '{ip}'. Port: '{port}'");
+            bool openServer = true;
+            while (openServer)
+            {
+                if (socketServer.IsNewData())
+                {
+                    var data = socketServer.GetData();
+                    // Do whatever you want with data
+                    Task.Run(() => DoSomething(data, socketServer));
+                }
+            }
+
+            //.... 
+            socketServer.CloseServer();
+        }
+
+        private static void DoSomething(KeyValuePair<TcpClient, byte[]> data, SocketServer server)
+        {
+            Console.WriteLine(((IPEndPoint)data.Key.Client.RemoteEndPoint).Address.ToString() + ": " + Encoding.UTF8.GetString(data.Value));
+            server.ResponseToClient(data.Key, "received");
+        }
+    }
+}
